b5c17c97461f53826923ceaf49ea0156d2fdc92b
Yao Fang <flavioy@live.com>
2020-10-19 16:21:26 +0800

    2.创建用户模块应用

---
+++ README.md
@@ -0,0 +1,12 @@
+### 1. 工程目录
+- `statics`:前端静态文件
+- `project`: 项目文件
+    - `apps`: 专门管理子应用的包
+    - `project`: 项目
+        -  `logs`: 用于存放日志文件
+        - `settings`: 设置文件
+            - `dev.py`: 开发时使用的配置文件
+            - `prod.py`: 项目上线使用的配置文件
+        - `templates`: 存放模板
+        -  `utils`: 工具目录
+ 
--- project/manage.py
+++ project/manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+    # 设置项目的setting文件
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
