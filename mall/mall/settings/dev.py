4c1c3d5ddccaa4c24ea827d5b643bd1d3eb89932
Yao Fang <flavioy@live.com>
2020-10-25 12:24:00 +0800

    14.多账号登陆实现

--- mall/mall/settings/dev.py
+++ mall/mall/settings/dev.py
@@ -17,6 +17,8 @@ import os,sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 指定自定义的用户认证后端
+AUTHENTICATION_BACKENDS = ['users.utils.UsernameMobileAuthBackend']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
