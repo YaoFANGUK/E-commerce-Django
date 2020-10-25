1f44d612b5eaa687df445a94c65f09f449b6fdea
Yao Fang <flavioy@live.com>
2020-10-25 10:07:58 +0800

    make migraions

--- mall/mall/apps/oauth/models.py
+++ mall/mall/apps/oauth/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from mall.utils.models import BaseModel
 
-# Create your models here.
+
+# 定义QQ登录的模型类:
+class OAuthQQUser(BaseModel):
+    """QQ登录用户数据"""
+
+    # user 是个外键, 关联对应的用户
+    user = models.ForeignKey('users.User',  on_delete=models.CASCADE, verbose_name='用户')
+    # qq 发布的用户身份id
+    openid = models.CharField(max_length=64,  verbose_name='openid', db_index=True)
+
+    class Meta:
+        db_table = 'tb_oauth_qq'
