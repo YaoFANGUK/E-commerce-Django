a56025680d874a51ffb6a896679193a4bce0af1e
Yao Fang <flavioy@live.com>
2020-10-28 12:16:09 +0800

    21.获取省份数据

--- mall/mall/apps/areas/views.py
+++ mall/mall/apps/areas/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from areas.models import Area
+from django.db import DatabaseError
 
-# Create your views here.
+class ProvinceAreasView(View):
+    """省级地区"""
+    def get(self, request):
+        """
+        提供省级地区数据:
+        1. 查询省级数据
+        2. 序列化省级数据
+        3. 响应省级数据
+        """
+        try:
+            # 1.查询省级数据
+            province_model_list = Area.objects.filter(parent__isnull=True)
+            # 2.整理省级数据
+            province_list = []
+            for province_model in province_model_list:
+                province_list.append({
+                    'id': province_model.id,
+                    'name': province_model.name
+                })
+        except DatabaseError as e:
+            return JsonResponse({
+                'code': 500,
+                'errmsg': '数据库获取省份数据失败'
+            })
+
+        # 3.返回整理好后的数据
+        return JsonResponse({
+            'code': 0,
+            'errmsg': 'ok',
+            'province_list': province_list
+        })
+
+
+
