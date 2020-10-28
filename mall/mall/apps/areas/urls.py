ebda297a5408717fd67ecb5b60fae1177135951d
Yao Fang <flavioy@live.com>
2020-10-28 15:36:56 +0800

    22.获取市区数据

--- mall/mall/apps/areas/urls.py
+++ mall/mall/apps/areas/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 省级地区
     path('areas/', views.ProvinceAreasView.as_view()),
+    # 子级地区
+    path('areas/<int:pk>/', views.SubAreasView.as_view()),
 ]
--- mall/mall/apps/areas/views.py
+++ mall/mall/apps/areas/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from areas.models import Area
 from django.db import DatabaseError
 
+
 class ProvinceAreasView(View):
     """省级地区"""
     def get(self, request):
@@ -23,7 +24,7 @@ class ProvinceAreasView(View):
                     'id': province_model.id,
                     'name': province_model.name
                 })
-        except DatabaseError as e:
+        except DatabaseError:
             return JsonResponse({
                 'code': 500,
                 'errmsg': '数据库获取省份数据失败'
@@ -37,4 +38,45 @@ class ProvinceAreasView(View):
         })
 
 
+class SubAreasView(View):
+    """
+    子级地区：市和区县
+    """
+    def get(self, request, pk):
+        """
+        提供市或区地区数据
+        1. 查询市或区数据
+        2. 序列化市或区数据
+        3. 响应市或区数据
+        """
+        try:
+            # 1.查询市或区数据
+            sub_model_list = Area.objects.filter(parent=pk)
+            # 查询省份数据
+            parent_model = Area.objects.get(id=pk)
+            # 2.整理市或区数据
+            sub_list = []
+            for sub_model in sub_model_list:
+                sub_list.append({
+                    'id': sub_model.id,
+                    'name': sub_model.name
+                })
 
+            sub_data = {
+                'id': parent_model.id,  # pk
+                'name': parent_model.name,
+                'subs': sub_list
+            }
+
+        except DatabaseError:
+            return JsonResponse({
+                'code': 500,
+                'errmsg': '数据库查询城市或区县数据出错',
+            })
+
+        # 3.响应市或区数据
+        return JsonResponse({
+            'code': 0,
+            'errmsg': 'ok',
+            'sub_data': sub_data
+        })
