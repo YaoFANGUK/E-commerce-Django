ef4d45da68480f504443dee6b791566702248b49
Yao Fang <flavioy@live.com>
2020-11-19 08:55:49 +0800

    add url

--- mall/mall/apps/payment/urls.py
+++ mall/mall/apps/payment/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('payment/<int:order_id>/', views.PaymentView.as_view()),
+    # 处理支付成功的回调： PUT
+    path('payment/status/', views.PaymentStatusView.as_view()),
 ]
